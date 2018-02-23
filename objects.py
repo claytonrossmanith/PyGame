35334a357f071f92fd1f2a3b5a3358bbc3cd9bce
19gavin <36310143+19gavin@users.noreply.github.com>
2018-02-23 14:07:44 -0600

    Add files via upload

--- objects.py
+++ objects.py
@@ -1,66 +1,33 @@
 import table
 
 class Obj:
-    def __init__(self, table, width = 100, height = 15, x_posn = 50,
-                 y_posn = 50, color = "red", x_speed = 50, y_speed = 50):
+    def __init__(self, table, width = 15, height = 100, x_posn = 50,
+                 y_posn = 50, color = "green", x_speed = 23, y_speed = 23):
+
         self.width = width
         self.height = height
         self.x_posn = x_posn
+        self.y_posn = y_posn
         self.color = color
         self.x_start = x_posn
         self.y_start = y_posn
         self.x_speed = x_speed
         self.y_speed = y_speed
         self.table = table
-        self.square = self.table.draw_square(self)
+        self.rectangle = self.table.draw_rectangle(self)
 
-    def move_up(self, master):
-        self.y_posn = self.y_posn - self.y_speed
-        if(self.y_posn <= 0):
-            self.y_posn = 0
-        x1 = self.x_posn
-        x2 = self.x_posn+self.width
-        y1 = self.y_posn
-        y2 = self.y_posn+self.height
-        self.table.move_item(self.square, x1, y1, x2, y2)
-
-    def move_down(self, master):
-        self.y_posn = self.y_posn + self.y_speed
-        far_bottom = self.table.height - self.height
-        if(self.y_posn >= far_bottom):
-            self.y_posn = far_bottom
-        x1 = self.x_posn
-        x2 = self.x_posn+self.width
-        y1 = self.y_posn
-        y2 = self.y_posn+self.height
-        self.table.move_item(self.square, x1, y1, x2, y2)
-
-    def move_right(self, master):
-        self.x_posn = self.x_posn + self.x_speed
-        if(self.x_posn >= far_right):
-            self.x_posn = far_right
-        x1 = self.x_posn
-        x2 = self.x_posn+self.width
-        y1 = self.y_posn
-        y2 = self.y_posn+self.height
-        self.table.move_square(self.square, x1, y1, x2, y2)
-
-    def move_right(self, master):
-        self.x_posn = self.x_posn + self.x_speed
-        if(self.x_posn >= far_right):
-            self.x_posn = far_right
-        x1 = self.x_posn
-        x2 = self.x_posn+self.width
-        y1 = self.y_posn
-        y2 = self.y_posn+self.height
-        self.table.move_square(self.square, x1, y1, x2, y2)
+    def start_objects(self, x_speed, y_speed):
+        self.x_speed = x_speed if random.randint(0, 1) else x_speed
+        self.y_speed = y_speed if random.randint(0, 1) else y_speed
+        self,start_position()
+        
 
     def start_position(self):
         self.x_posn = self.x_start
         self.y_posn = self.y_start
 
-    def detect_collision(self, ball, sides_sweet_spot = True, topnbottom_sweet_spot = False):
-        collision_direction = " "
+    def detect_collision(self, ball, topnbottom_sweet_spot=True, side_sweet_spot=False):
+        collision_direction = ""
         collision = False
         feel = 5
         top = self.y_posn
@@ -69,16 +36,40 @@ class Obj:
         right = self.x_posn + self.width
         v_center = top + (self.height/2)
         h_center = left + (self.width/2)
-
-        if(x_posn <= 3):
+        top_b = ball.y_posn
+        bottom_b = ball.y_posn + ball.height
+        left_b = ball.x_posn
+        right_b = ball.x_posn + ball.width
+        r = (right_b - left_b)/2
+        v_center_b = top_b + r
+        h_center_b = left_b + r
+        if((bottom_b > top) and (top_b < bottom) and (right_b > left) and (left_b < right)):
             collision = True
-            collision_direction = "E"
-        if(x_posn >= 997):
+            print("collision")
+
+        if((bottom_b > top) and (top_b < bottom) and (right_b > left) and (left_b < right)):
             collision = True
-            collision_direction = "W"
-        
+        if(collision):
+            if((top_b > top-r) and (bottom_b < bottom + r) and (right_b > right) and (left_b <=right)):
+                collision_direction = "E"
+                ball.x_speed = abs(ball.x_speed)
 
-            
-        
+            elif((left_b > left-r) and (right_b < right+r) and (bottom_b > bottom) and (top_b <= bottom)):
+                collision_direction = "S"
+                ball.y_speed = abs(ball.y_speed)
 
-        
+            elif((left_b > left-r) and (right_b < right+r) and (top_b < top) and (bottom_b >= top)):
+                collision_direction = "N"
+                ball.y_speed = -abs(ball.y_speed)
+            elif((top_b > top-r) and (bottom_b < bottom+r) and (left_b < left) and (right_b >= left)):
+                collision_direction = "W"
+                ball.x_speed = -abs(ball.x_speed)
+            else:
+                collision_direction = "miss"
+            if((sides_sweet_spot == True) and (collision_direction == "W" or collision_direction == "E")):
+                adjustment = (-(v_center - v_center_b))/(self.width/2)
+                ball.x_speed = feel * adjustment
+            if((topnbottom_sweet_spot == True) and (collision_direction == "N" or collision_direction == "S")):
+                adjustment = (-(h_center - h_center_b))/(self.width/2)
+                ball.x_speed = feel * adjustment
+            return (collision, collision_direction)
